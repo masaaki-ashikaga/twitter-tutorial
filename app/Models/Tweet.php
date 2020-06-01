e198c4cbf89995baa8d0d24a0daacbc1035376ad
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-01 21:00:32 +0900

    ツイート一覧表示

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -36,4 +36,11 @@ class Tweet extends Model
     {
         return $this->where('user_id', $user_id)->count();
     }
+
+    public function getTimeLines(Int $user_id, Array $follow_ids)
+    {
+        //自身とフォローしているユーザIDを結合する
+        $follow_ids[] = $user_id;
+        return $this->whereIn('user_id', $follow_ids)->orderBy('created_at', 'DESC')->paginate(50);
+    }
 }
