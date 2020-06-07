a20063a7f9c753d897db0dc68050d63e768d202f
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-06-07 23:28:05 +0900

    いいね機能実装

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -7,4 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     public $timestamps = false;
+
+    //いいねしているかどうか判断処理
+    public function isFavorite(Int $user_id, Int $tweet_id)
+    {
+        return (boolean) $this->where('user_id', $user_id)->where('tweet_id', $tweet_id)->first();
+    }
+
+    //いいね登録
+    public function storeFavorite(Int $user_id, Int $tweet_id)
+    {
+        $this->user_id = $user_id;
+        $this->tweet_id = $tweet_id;
+        $this->save();
+
+        return;
+    }
+
+    //いいね削除
+    public function destroyFavorite(Int $favorite_id)
+    {
+        return $this->where('id', $favorite_id)->delete();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,10 @@ Route::delete('users/{user}/unfollow', 'UsersController@unfollow')->name('unfoll
 
 //ツイート関連
 Route::resource('tweets', 'TweetsController', ['only' => ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']]);
+
+//コメント関連
+Route::resource('comments', 'CommentsController', ['only' => ['store']]);
+
+//いいね関連
+Route::resource('favorites', 'FavoritesController', ['only' => ['store', 'destroy']]);
 });
